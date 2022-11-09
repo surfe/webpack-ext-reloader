565a12bc1e92232e4f6bee6806665fd442ad3a9c
Rui Xavier Costa <ruixaviercosta@gmail.com>
2022-11-09 15:46:50 +0100

    Updated middleware to be compatible with MV3 service worker

--- sample/src/my-content-script.js
+++ sample/src/my-content-script.js
@@ -2,3 +2,7 @@ import depSample from "./dependency-sample";
 
 console.info("Change anything here!! It will hot-reload the extension :)");
 console.log(depSample());
+
+const title = document.createElement("h1");
+title.innerHTML = "test :D";
+document.body.prepend(title);
